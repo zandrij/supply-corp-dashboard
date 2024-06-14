f211a819bf752a0a5129a90035892248e2a8c8f0
zandrij <ajosuesh@gmail.com>
2024-06-14 18:26:17 -0400

    fix: compile

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "../globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-export function DashboardPage() {
+export default function DashboardPage() {
     return (
         <>dashboard</>
     );
